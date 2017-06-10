54e4fa52b3fd5bd50d509650a0855f439632389b
durswd <swda.durl@gmail.com>
2017-06-11 00:21:20 +0900

    Initial commit

---
+++ docs/Sample/grimoirejs-effekseer.js
@@ -0,0 +1,56 @@
+gr.registerComponent("EffekseerRenderer",{
+  effects:{},
+  attributes:{
+  },
+  $awake:function(){
+    this._gl = this.companion.get("gl");
+    this._canvas = this.companion.get("canvasElement");
+    effekseer.init(this._gl);
+  },
+  $mount:function(){
+  },
+  $update:function(){
+  },
+  $render:function(args){
+    this._gl.viewport(args.viewport.Left, this._canvas.height - args.viewport.Bottom, args.viewport.Width, args.viewport.Height);
+    
+    effekseer.update();
+    effekseer.setProjectionMatrix(args.camera.ProjectionMatrix.rawElements);
+    effekseer.setCameraMatrix(args.camera.ViewMatrix.rawElements);
+    effekseer.draw();
+  }
+});
+gr.registerNode("render-effekseer",["EffekseerRenderer"],{},"");
+
+gr.registerComponent("EffekseerEmitter",{
+  attributes:{
+    src: {converter: "String", default: null},
+  },
+  $mount:function() {
+    var src = this.getAttribute("src");
+    if (src) {
+      this.effect = effekseer.loadEffect(src, 
+        function(){
+          this.handle = effekseer.play(this.effect);
+        }.bind(this)
+      );
+    }
+  },
+  $update:function() {
+    if("handle" in this && this.handle != null)
+    {
+      var transform = this.node.getComponent("Transform");
+
+      var pos = transform.getAttribute("position");
+      this.handle.setLocation(pos.X, pos.Y, pos.Z);
+
+      const rot = transform.getAttribute("rotation").eularAngles;
+      this.handle.setRotation(rot.X,rot.Y,rot.Z);
+
+      const s = transform.getAttribute("scale");
+      this.handle.setScale(s.X,s.Y,s.Z);
+
+    }
+  },
+});
+gr.registerNode("efk",["EffekseerEmitter"],{},"mesh");
---
+++ docs/Sample/sample01.js
@@ -0,0 +1,20 @@
+
+gr.registerComponent("Rotate",{
+  attributes:{
+    speed:
+    {
+      default:1,
+      converter:"Number"
+    }
+  },
+  $mount:function(){
+    this.phi = 0;
+  },
+  $update:function(){
+    this.phi += this.getAttribute("speed");
+    this.node.setAttribute("rotation",this.phi + "," + this.phi + "," + this.phi);
+  }
+});
+gr.registerNode("rotated-mesh",["Rotate"],{},"mesh");
+
+
